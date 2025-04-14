2475afa071682b794bc0af80e315e9b1d4913eae
Scott Carlow <me@scottcarlow.com>
2025-04-13 23:33:45 -0700

    Change some home page copy

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Your Name';
-export const SITE_DESCRIPTION = 'Personal website and blog - Software Engineer & Web Developer';
+export const SITE_TITLE = 'Commit to the bit';
+export const SITE_DESCRIPTION = 'Personal website and blog - IT Engineer & Leader';
